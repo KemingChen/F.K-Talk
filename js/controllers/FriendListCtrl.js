61e011294cd9f1c3731131a74ec4b0e52260a7d2
KemingChen <believe75467@gmail.com>
2014-04-23 15:10:58 +0800

    finish search

--- js/controllers/FriendListCtrl.js
+++ js/controllers/FriendListCtrl.js
@@ -1,23 +1,36 @@
 app.controller('FriendListCtrl', function($scope, HostManager){
 	HostManager.checkLogin();
-	$scope.friends = [
+	$scope.filterFriends = [];
+	$scope.search = "";
+	// test
+	var friends = [
 		{
-			"name": "陳科銘",
-			"phone": "0961276368",
-			"photo": "images/0961276368.jpg",
-			"email": "believe75467@gmail.com"
+			name: "陳科銘",
+			phone: "0961276368",
+			photo: "images/NoPhoto.jpg",
+			mail: "believe75467@gmail.com"
 		},
 		{
-			"name": "謝宗廷",
-			"phone": "0987103180",
-			"photo": "images/0987103180.jpg",
-			"email": "gary62107@gmail.com"
+			name: "謝宗廷",
+			phone: "0987103180",
+			photo: "images/NoPhoto.jpg",
+			mail: "gary62107@gmail.com"
 		},
 		{
-			"name": "陳英一",
-			"phone": "0912345678",
-			"photo": "images/NoPhoto.jpg",
-			"email": "EnglishOne@gmail.com"
+			name: "陳英一",
+			phone: "0912345678",
+			photo: "images/NoPhoto.jpg",
+			email: "EnglishOne@gmail.com"
 		}
-	]
+	];
+	// test end
+
+	$scope.filter = function(key){
+		$scope.filterFriends = [];
+		angular.forEach(friends, function(obj){
+			if(key == "" || obj.name.match(key) !== null || obj.phone.match(key) !== null){
+				$scope.filterFriends.push(obj);
+			}
+		});
+	};
 });
